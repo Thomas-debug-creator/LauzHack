a804a71b75e04f37016e4eef6bb95e6be9219545
utsav-akhaury <f2016684@pilani.bits-pilani.ac.in>
2022-12-03 18:25:42 +0100

    Temp control curve

--- heating_regulation_system.py
+++ heating_regulation_system.py
@@ -3,7 +3,22 @@ import numpy as np
 
 class HeatingRegulationSystem:
 
-    def __init__(self, T_min, T_ideal) -> None: 
+    def __init__(self, room, T_min=15, T_ideal=25) -> None:
+        self.room = room 
         self.T_min = T_min
         self.T_ideal = T_ideal
-        # self.heat_regulation_curve = 
+        self.heat_regulation_curve = np.zeros((24*60))   # 1-minute quantization
+    
+    def control_heating(self):
+        for i in range(len(self.room.bookings)):
+            t_start = self.room.bookings[i].t_start
+            t_end = self.room.bookings[i].t_end
+
+            self.heat_regulation_curve[int(t_start):int(t_end)] = 1
+
+        return self.heat_regulation_curve
+
+    def plot_heat_regulation_curve(self):
+        plt.figure()
+        plt.plot(self.heat_regulation_curve)
+        plt.show()
