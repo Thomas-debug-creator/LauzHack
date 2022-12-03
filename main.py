51a0450e0aac9d1487372796fa87b7071ee4b70c
Thomas Rimbot <thomas.rimbot@mail.polimi.it>
2022-12-03 15:25:07 +0100

    Update Timetable abstraction

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ def main():
     print('Starting the program')
 
     # Generate random timetable data
-    nb_rooms = 2
-    data_tt = np.random.randint(-2,5,(nb_rooms,24))
-    data_tt[data_tt < 0] = 0
+    room_1 = Room("BC410")
+    room_2 = Room("BC420")
+
+    room_1.add_booking(RoomBooking(1.5,2.5,4))
+    room_1.add_booking(RoomBooking(4,5,2))
+    room_2.add_booking(RoomBooking(5,8,1))
+
+    data_tt = {room_1, room_2}
 
     # Put it inside Timetable object
     tt = Timetable(data_tt)
 
     # Display data
-    print(tt.data)
+    tt.display()
 
 
 if __name__ == '__main__':
--- timetable.py
+++ timetable.py
@@ -1,11 +1,62 @@
+class Room:
+    def __init__(self, id) -> None:
+        self.id = id
+        self.bookings = []
+
+    def add_booking(self, new_booking):
+        conflict = False
+        for booking in self.bookings:
+            if booking.check_conflict(new_booking):
+                conflict = True
+                break
+        if not conflict:
+            self.bookings.append(new_booking)
+        else:
+            print("WARNING : cannot add the booking because of conflicts")
+
+    def display(self):
+        print('Room', self.id, ': ')
+        for booking in self.bookings:
+            booking.display()
+
+    
+
+class RoomBooking:
+    def __init__(self, t_start, t_end, nb_people) -> None:
+        self.t_start = t_start
+        self.t_end = t_end
+        self.nb_people = nb_people
+
+    def display(self):
+        print(self.t_start, self.t_end, self.nb_people)
+
+    def check_conflict(self, other_booking):
+        if self.t_end <= other_booking.t_end and self.t_end > other_booking.t_start:
+            return True
+        elif self.t_start <= other_booking.t_start and self.t_end >= other_booking.t_end:
+            return True
+        elif other_booking.t_end <= self.t_end and other_booking.t_end > self.t_start:
+            return True
+        elif other_booking.t_start <= self.t_start and other_booking.t_end >= self.t_end:
+            return True
+        return False
+
 
 class Timetable:
-    def __init__(self, data) -> None:
-        self.data = data
+    def __init__(self, list_rooms) -> None:
+        # list_rooms = {Room}
+        self.rooms = {room.id: room for room in list_rooms}
 
-    def get_occupancy(self, room_id, slot_id):
-        return self.data[room_id, slot_id]
+    def get_room(self, room_id):
+        return self.rooms[room_id]
 
-    def change_occupancy(self, room_id, slot_id, new_occupancy):
-        self.data[room_id, slot_id] = new_occupancy
+    def add_room_booking(self, room_id, booking):
+        self.rooms[room_id].add_booking(booking)
+
+    def display_room(self, room_id):
+        self.rooms[room_id].display()
+
+    def display(self):
+        for room_id in self.rooms:
+            self.rooms[room_id].display()
     
