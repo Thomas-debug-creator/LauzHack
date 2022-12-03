7a60226fd8cc0a456946768e0915b005a9b2651b
utsav-akhaury <f2016684@pilani.bits-pilani.ac.in>
2022-12-03 18:37:04 +0100

    test

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from timetable import *
 from temperature import *
+from heating_regulation_system import *
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
     print('Starting the program')
+
+    # Generate random timetable data
+    room_1 = Room("BC410")
+    room_2 = Room("BC420")
+
+    room_1.add_booking(RoomBooking("BC410","U",1.5,2.5))
+    room_1.add_booking(RoomBooking("BC410","U",4,5))
+    room_2.add_booking(RoomBooking("BC410","U",5,8))
 
     # Get room information online
     list_rooms = ["BC410", "BC420"]
@@ -19,11 +28,16 @@ def main():
     T_hot = 20
     k = 1
 
-    time_span = np.arange(0,20)
-    temperature_evolution = TemperatureEvolution()
-    temperature_span = temperature_evolution.cooling_law(T_hot, T_cold, k, time_span)
-    plt.plot(time_span, temperature_span)
-    plt.show()
+    # time_span = np.arange(0,20)
+    # temperature_evolution = TemperatureEvolution()
+    # temperature_span = temperature_evolution.cooling_law(T_hot, T_cold, k, time_span)
+    # plt.plot(time_span, temperature_span)
+    # plt.show()
+
+    hrs = HeatingRegulationSystem(room_1)
+    hrs.control_heating()
+    hrs.plot_heat_regulation_curve()
+
 
 
 if __name__ == '__main__':
