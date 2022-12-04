0cfc217162d56a051d92065a52dc9e4c626eb940
Thomas Rimbot <thomas.rimbot@mail.polimi.it>
2022-12-04 09:39:35 +0100

    Add heating regulation system

--- heating_regulation_system.py
+++ heating_regulation_system.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 from timetable import *
+from temperature import *
+from math import floor
 
 
 class HeatingRegulationSystem:
 
-    def __init__(self, room, T_min=10, T_hot=20, warmup=15, cooldown=5) -> None:
+    def __init__(self, room, temperature_evolution, T_cool = 3, T_heat = 20) -> None:
         self.room = room 
-        self.T_min = T_min
-        self.T_hot = T_hot
-        self.warmup = warmup                             # minutes takes to attain desired temperature
-        self.cooldown = cooldown                             # minutes takes to attain desired temperature
+        self.temperature_evolution = temperature_evolution
         self.heat_regulation_curve = {} 
+        self.T_cool = T_cool
+        self.T_heat = T_heat
 
     def get_time_standard(self, room_booking):
         format  = "%Y-%m-%dT%H:%M:%S"
@@ -23,7 +24,53 @@ class HeatingRegulationSystem:
     def convert_to_minutes(self, time):
         return time.hour*60 + time.minute
 
+    def get_room_occupancy_in_mn(self):
+        times_occupancy = []
+        for i in range(len(self.room.bookings)):
+
+            # Get time in standard format
+            start, end = self.get_time_standard(self.room.bookings[i])
+
+            # Get times in minute format
+            start_time_mn = self.convert_to_minutes(start)
+            end_time_mn = self.convert_to_minutes(end)
+
+            times_occupancy.append(start_time_mn)
+            times_occupancy.append(end_time_mn)
+
+        self.times_occupancy = np.array(times_occupancy)
+
+    def compute_time_shifts(self):
+        # Assume the first shits is a switch-on (i.e we start heating)
+        T_start = self.T_cool
+        T_end = self.T_heat
+
+        time_shifts = [0]
+        t_star = 0
+
+        for i in range(len(self.times_occupancy)):
+            # Compute the times based on previous times and temperatures
+            if i % 2 == 0: # heating time
+                T_end = self.T_heat
+                t_star = temperature_evolution.compute_warmup_time(self.T_heat, self.T_cool, T_start, time_shifts[-1], self.times_occupancy[i])
+            else: # cooling time
+                T_end = self.T_cool
+                t_star = temperature_evolution.compute_cooldown_time(self.T_heat, self.T_cool, T_start, time_shifts[-1], self.times_occupancy[i])
+
+            time_shifts.append(floor(t_star))
+
+            # Simulate for the considered period to get the new starting temperature
+            time_span_period, temperature_span_period = temperature_evolution.simulate_heat_transfer_over_period(T_start, T_end, time_shifts[-2], time_shifts[-1])
+            T_start = temperature_span_period[-1]
+
+        #    Remove the first entry of the time shifts corresponding to the start of the time frame
+        self.time_shifts = np.array(time_shifts[1:])
+
+
+
     def control_heating(self):
+        self.get_room_occupancy_in_mn()
+        self.compute_time_shifts()
 
         for i in range(len(self.room.bookings)):
 
@@ -33,11 +80,15 @@ class HeatingRegulationSystem:
             # Get time in standard format
             start, end = self.get_time_standard(self.room.bookings[i])
 
+            # Compute warmump and cooldown times for this period
+            warmup_time = self.time_shifts[2*i]
+            cooldown_time = self.time_shifts[2*i + 1]
+
             key = 'Room: ' + self.room.id + '  |  Date: ' + datetime.datetime.fromtimestamp(t_start_unix).strftime("%d %b, %Y")
             if key not in self.heat_regulation_curve.keys():
                 self.heat_regulation_curve[key] = np.zeros((24*60))
 
-            self.heat_regulation_curve[key][int(self.convert_to_minutes(start) - self.warmup) : int(self.convert_to_minutes(end) - self.cooldown)] = 1
+            self.heat_regulation_curve[key][int(self.convert_to_minutes(start) - warmup_time) : int(self.convert_to_minutes(end) - cooldown_time)] = 1
                
         return self.heat_regulation_curve
 
